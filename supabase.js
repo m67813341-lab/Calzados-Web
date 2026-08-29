6da118f0a46dbb5356a4d3c3275cea81cda5ad92
m67813341-lab <m67813341@gmail.com>
2026-08-29 20:41:25 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://mepksofzezyuzwqflaew.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_QG8WdtMJr3ZtSqpIzisGgw_eyHu5Acv";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
